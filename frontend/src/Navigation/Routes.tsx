0396b8095cd96f3291b57ac265c52ab80d88d4bd
stinodes <stijn@stinodes.dev>
2024-04-04 00:10:14 +0200

    changed to hashrouter for pages

--- frontend/src/Navigation/Routes.tsx
+++ frontend/src/Navigation/Routes.tsx
@@ -1,11 +1,11 @@
-import { Route, Router } from '@solidjs/router'
+import { HashRouter, Route, Router } from '@solidjs/router'
 import { Layout } from './Layout'
 import { Search } from '../search'
 
 export const Routes = (props: {}) => {
   return (
-    <Router root={Layout} base={import.meta.env.BASE_URL}>
+    <HashRouter root={Layout} base={import.meta.env.BASE_URL}>
       <Route path="/search" component={Search} />
-    </Router>
+    </HashRouter>
   )
 }
